058dec622d07d7b1e56ef70a117f1d9b84be43b1
Ram-Indla <ramakrishna.aws2026@gmail.com>
2026-07-11 03:41:17 +0530

    added installations.sh file in repo

---
+++ 02-installations.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+Package=$@
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "Please try with super user"
+    exit 1    # here "exit 1" is Manually exit
+else
+    echo "You are super user"
+fi
+
+validate_function(){
+if [ $1 -ne 0 ]
+then
+    echo "Installing $2 ...."
+    dnf install $2 -y
+else
+    echo "$2 is already installed"
+fi
+}
+
+for i in $Package
+do
+    echo "listing $i"
+    rpm -q $i
+    validate_function $? "$i"
+done
+
+
