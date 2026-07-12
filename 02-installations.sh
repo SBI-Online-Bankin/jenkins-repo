daf65ec89454ae5e2318cb1a11ae2ce69c0fcebd
Ram-Indla <ramakrishna.aws2026@gmail.com>
2026-07-12 11:41:08 +0530

    added sudo in installations.sh file in repo

--- 02-installations.sh
+++ 02-installations.sh
@@ -3,8 +3,8 @@ Package=$@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo "Please try with super user"
-    exit 1    # here "exit 1" is Manually exit
+    echo "Please try with super user or use sudo previlises"
+    # exit 1    # here "exit 1" is Manually exit
 else
     echo "You are super user"
 fi
